0783fda2396da1b7e197448bd04b491f4cc9400e
Giovanna Ruggio <giovannaruggio@gmail.com>
2022-02-17 16:17:36 -0500

    Add commenting

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,9 +12,8 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  // console.log('PUT to the database');
 
   const jateDb = await openDB('jate', 1);
 
@@ -26,10 +25,9 @@ export const putDb = async (content) => {
 
   const result = await request;
 
-  // console.log('🚀 - data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Add logic for a method that gets all the content from the database
 export const getDb = async () => {
 
   const jateDb = await openDB('jate', 1);
