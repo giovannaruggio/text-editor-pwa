28e5b6d3ee029cf71a39a57569fbd45a81e99496
Giovanna Ruggio <giovannaruggio@gmail.com>
2022-02-17 14:55:34 -0500

    Fix to reveal header

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('PUT to the database');
+  // console.log('PUT to the database');
 
   const jateDb = await openDB('jate', 1);
 
@@ -22,16 +22,15 @@ export const putDb = async (content) => {
 
   const store = tx.objectStore('jate');
 
-  const request = store.put({ id: id, jate: content });
+  const request = store.put({ id: 1, jate: content });
 
   const result = await request;
 
-  console.log('🚀 - data saved to the database', result);
+  // console.log('🚀 - data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('GET from the database');
 
   const jateDb = await openDB('jate', 1);
 
@@ -39,11 +38,11 @@ export const getDb = async () => {
 
   const store = tx.objectStore('jate');
 
-  const request = store.getAll();
+  const request = store.get(1).content;
 
   const result = await request;
-  console.log('result.value', result);
-  return result;
+
+  return result?.value;
 };
 
 initdb();
